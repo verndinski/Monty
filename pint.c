5029cd23cf149438d95781e4532a77d3faebd242
verndinski <zainazayuni@gmail.com>
2024-01-21 06:53:58 -0800

    pint.c

--- pint.c
+++ pint.c
@@ -1,35 +1,15 @@
-class MiniBytecodeInterpreter:
-    def __init__(self):
-        self.stack = []
+/**
+ * pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: double pointer to the top node of the stack.
+ * @line_number: the line number of the bytecode file.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-    def push(self, arg):
-        try:
-            num = int(arg)
-        except ValueError:
-            print(f"L{self.line_number}: usage: push integer")
-            exit(1)
-        self.stack.append(num)
-
-    def pint(self):
-        if not self.stack:
-            print(f"L{self.line_number}: can't pint, stack empty")
-            exit(1)
-        print(self.stack.pop())
-
-    def pall(self):
-        for num in reversed(self.stack):
-            print(num)
-        self.stack.clear()
-
-    def run(self, bytecode, line_number):
-        self.line_number = line_number
-        for instruction in bytecode:
-            opcode, arg = instruction
-            if opcode == "push":
-                self.push(arg)
-            elif opcode == "pint":
-                self.pint()
-            elif opcode == "pall":
-                self.pall()
-            else:
-                raise ValueError(f"Unknown opcode: {opcode}")
+    printf("%d\n", (*stack)->n);
+}
